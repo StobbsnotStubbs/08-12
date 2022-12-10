f24c8829be1fe1d92068a1d84580f4d97f7eea8c
StobbsnotStubbs <118483248+StobbsnotStubbs@users.noreply.github.com>
2022-12-10 12:37:14 +0000

    javascript exercise 1

---
+++ 08 12/main.js
@@ -0,0 +1,14 @@
+let age = 28;
+console.log(age);
+
+let numberOfRabbits = 2;
+numberOfRabbits = numberOfRabbits * 3
+console.log(numberOfRabbits)
+
+let bodmasExample = 3;
+bodmasExample = (bodmasExample + 5) / 6
+console.log(bodmasExample)
+
+
+randomNum = Math.floor(Math.random() * 6 + 1)
+console.log(randomNum)
